ff6a3cde3594ca7d18c7a389e2647bf60f9d0c2f
Your Name <you@example.com>
2023-11-06 18:04:22 -0500

    test server

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.send("test rateMyAgency");
+app.get("/", function (req, res) {
+    res.redirect("/index");
 });
 
 // setup http server to listen on HTTP_PORT
