4745e4994cde0d2c266752dc9e86a61b451c5bec
Your Name <you@example.com>
2023-11-13 23:44:05 -0500

    minor edit

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static(path.join(__dirname,'/index.html')));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"))
-});
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/index.html"))
+// });
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
