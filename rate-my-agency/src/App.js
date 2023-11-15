fbfcd9e121ea1530523f537a1c261704abc0d8f8
Your Name <you@example.com>
2023-11-14 21:08:01 -0500

    react

---
+++ rate-my-agency/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const SchoolLookup = () => {
+  const [isInfoName, setIsInfoName] = useState(true);
+  const [placeholder, setPlaceholder] = useState("School's name");
+  const [displayText, setDisplayText] = useState("I'd like to look up my Agency by name");
+
+  const handleLookupClick = () => {
+    if (isInfoName) {
+      setDisplayText("I'd like to look up my Agency by school");
+      setPlaceholder("Agency's name");
+    } else {
+      setDisplayText("I'd like to look up my Agency by name");
+      setPlaceholder("School's name");
+    }
+
+    setIsInfoName(!isInfoName);
+  };
+
+  return (
+    <div id="info" onClick={handleLookupClick}>
+      {displayText}
+      <input id="input" type="text" placeholder={placeholder} />
+    </div>
+  );
+};
+
+export default SchoolLookup;
