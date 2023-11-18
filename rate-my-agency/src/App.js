a0dad6597550a4cffe6c1ed2deb5d2f76cbe315a
Your Name <you@example.com>
2023-11-18 01:43:52 -0500

    update input

--- rate-my-agency/src/App.js
+++ rate-my-agency/src/App.js
@@ -3,69 +3,65 @@ import './style.css';
 
 function App() {
   // search type
-  const [search,setSearch] = useState(true);
+  const [search, setSearch] = useState(true);
   // input
+  const [input, setInput] = useState('')
   const [agency, setAgency] = useState('');
-  const [school,setSchool] = useState('');
+  const [school, setSchool] = useState('');
   // text -> combine to array later
-  const [info,setInfo] = useState("I'd like to look up my Agency by 'school'");
-  const [text,setText] = useState('Agency');
-  const [placeholder,setPlaceholder] = useState("Agency's name");
+  const [info, setInfo] = useState("I'd like to look up my Agency by 'school'");
+  const [text, setText] = useState('Agency');
+  const [placeholder, setPlaceholder] = useState("Agency's name");
 
-const handleClick = () => {
-  if(search) {
-    setText("Agency");
-    setPlaceholder("Agency's name");
-    setInfo("I'd like to look up my Agency by 'school'");
-  } else {
-    setText("School")
-    setPlaceholder("School's name");
-    setInfo("I'd like to look up my Agency by 'name'");
+  const handleClick = () => {
+    if (search) {
+      setText("Agency");
+      setPlaceholder("Agency's name");
+      setInfo("I'd like to look up my Agency by 'school'");
+    } else {
+      setText("School")
+      setPlaceholder("School's name");
+      setInfo("I'd like to look up my Agency by 'name'");
+    }
+    setSearch(!search);
   }
-  setSearch(!search);
-  
-}
 
-return (
-  <div>
-  <h2 className='text'>Enter your <span id="emAgency">{text}</span> to get start</h2>
-  <input className='input'
-  value={agency}
-  onChange={event => setAgency(agency)}
-  placeholder={placeholder}
-  type='text'
-  />
-  <p onClick={handleClick} id='info' className='infoName'>{info}</p>
-  </div>
-);
+  const searchInput = (event) => {
+    setInput(event.target.value);
+    if (event.key === 'Enter') {
+      // test
+
+      // if search == true -> agency
+      // false -> school
+      console.log(search);
+      console.log(input);
+
+      if (search === true) {
+        // set agency
+        setAgency(input);
+        console.log(agency);
+      } else {
+        // set school
+        setSchool(input);
+        console.log(school);
+      }
+
+    }
+  }
+
+  return (
+    <div>
+      <h2 className='text'>Enter your <span id="emAgency">{text}</span> to get start</h2>
+      <input className='input'
+        value={input}
+        onChange={searchInput}
+        onKeyDown={searchInput}
+        placeholder={placeholder}
+        type='text'
+      />
+      <p onClick={handleClick} id='info' className='infoName'>{info}</p>
+    </div>
+  );
 }
 
 export default App;
-
-// const SchoolLookup = () => {
-//   const [isInfoName, setIsInfoName] = useState(true);
-//   const [placeholder, setPlaceholder] = useState("School's name");
-//   const [displayText, setDisplayText] = useState("I'd like to look up my Agency by name");
-
-//   const handleLookupClick = () => {
-    
-//     if (isInfoName) {
-//       setDisplayText("I'd like to look up my Agency by school");
-//       setPlaceholder("Agency's name");
-//     } else {
-//       setDisplayText("I'd like to look up my Agency by name");
-//       setPlaceholder("School's name");
-//     }
-
-//     setIsInfoName(!isInfoName);
-//   };
-
-//   return (
-//     <div id="info" onClick={handleLookupClick}>
-//       {displayText}
-//       <input id="input" type="text" placeholder={placeholder} />
-//     </div>
-//   );
-// };
-
-// export default SchoolLookup;
