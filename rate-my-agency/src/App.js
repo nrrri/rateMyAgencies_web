dee8761f24ddf2bbd005569b37d7481292357111
Your Name <you@example.com>
2023-11-18 00:37:12 -0500

    update reach code

--- rate-my-agency/src/App.js
+++ rate-my-agency/src/App.js
@@ -1,29 +1,59 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const SchoolLookup = () => {
-  const [isInfoName, setIsInfoName] = useState(true);
-  const [placeholder, setPlaceholder] = useState("School's name");
-  const [displayText, setDisplayText] = useState("I'd like to look up my Agency by name");
+function App() {
+  const [search,setSearch] = useState('');
+  const [agency, setAgency] = useState('');
+  const [school,setSchool] = useState('');
 
-  const handleLookupClick = () => {
-    if (isInfoName) {
-      setDisplayText("I'd like to look up my Agency by school");
-      setPlaceholder("Agency's name");
-    } else {
-      setDisplayText("I'd like to look up my Agency by name");
-      setPlaceholder("School's name");
-    }
+const SearchInput = (search) => {
+  if(search === 'Agency') {
+    setAgency(search);
+  } else if (search === 'School') {
+    setSchool(search);
+  }
+}
 
-    setIsInfoName(!isInfoName);
-  };
+return (
+  <div>
+  <h2 className='text'>Enter your <span id="emAgency">Agency</span> to get start</h2>
+  <input className='input'
+  value={agency}
+  onChange={event => setAgency(agency)}
+  placeholder="Agency's name"
+  type='text'
+  />
+  <p id='info' className='infoName'>I'd like to look up my Agency by school</p>
+  </div>
+);
+}
 
-  return (
-    <div id="info" onClick={handleLookupClick}>
-      {displayText}
-      <input id="input" type="text" placeholder={placeholder} />
-    </div>
-  );
-};
+export default App;
 
-export default SchoolLookup;
+// const SchoolLookup = () => {
+//   const [isInfoName, setIsInfoName] = useState(true);
+//   const [placeholder, setPlaceholder] = useState("School's name");
+//   const [displayText, setDisplayText] = useState("I'd like to look up my Agency by name");
+
+//   const handleLookupClick = () => {
+    
+//     if (isInfoName) {
+//       setDisplayText("I'd like to look up my Agency by school");
+//       setPlaceholder("Agency's name");
+//     } else {
+//       setDisplayText("I'd like to look up my Agency by name");
+//       setPlaceholder("School's name");
+//     }
+
+//     setIsInfoName(!isInfoName);
+//   };
+
+//   return (
+//     <div id="info" onClick={handleLookupClick}>
+//       {displayText}
+//       <input id="input" type="text" placeholder={placeholder} />
+//     </div>
+//   );
+// };
+
+// export default SchoolLookup;
